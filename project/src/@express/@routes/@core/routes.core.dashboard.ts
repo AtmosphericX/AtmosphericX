3ead5454b6e961137be615ef789cd660ea153c75
k3yomi <54733885+K3YOMI@users.noreply.github.com>
2026-03-04 15:17:46 -0600

    fix: remove unnecessary console log for documentation mode check in dashboard routes

--- project/src/@express/@routes/@core/routes.core.dashboard.ts
+++ project/src/@express/@routes/@core/routes.core.dashboard.ts
@@ -37,7 +37,6 @@ export class Init {
                 const isSetupFinished = loader.cache.external.setup == 0 ? false : true;
                 const isPortal = configurations.web_hosting_settings.is_login_required ?? false;
                 const documentation = configurations.web_hosting_settings?.documentation_mode ?? false;
-                console.log(documentation)
                 if (documentation) {
                     return response.redirect(`/documentation`);
                 }
