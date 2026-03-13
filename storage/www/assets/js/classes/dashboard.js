d0113ecb4ec1e5aab70bba146f5ce2075424d80b
Kiyo Flores <kiyo-flores@fbi.gov>
2026-03-13 17:28:01 -0500

    Refactor dashboard rendering and UI layout

--- storage/www/assets/js/classes/dashboard.js
+++ storage/www/assets/js/classes/dashboard.js
@@ -19,34 +19,24 @@ class Dashboard {
     constructor(utils) { 
         this.name_space = `webmodule:dashboard`;
         this.storage = utils.storage;
-        this.landing_page = `#home`;
-        this.static_pages = ['#ho2me']
-        window.history.pushState({}, null, this.landing_page); 
+        this.utils = utils;
+        window.history.pushState({}, null, `#home`); 
         utils.log(`${this.name_space} initialized.`);
         utils.socket([`*`]).then(() => {
             document.addEventListener('onUpdate', (event) => {this.onUpdate()});
         });
     }
     
-    /**
-     * @production
-     * @function getLandingPage
-     * @description 
-     *      Gets the landing page of the dashboard. This is used to see what part of the dashboard the user is currently on.
-     *      This is used to determine what content to show the user and to update the URL accordingly. 
-     *      The landing page is set to `#home` by default, but can be changed by the user. 
-     *      The landing page is also used to determine what content to show the user when they first load the dashboard.
-     * 
-     * @returns {string} The landing page of the dashboard.
-     */
-    getLandingPage() { return this.landing_page; }
     
-    
-    setLoadPage(page) { 
-        // wipe all contents from this page
-        const getPage = document.getElementById(page.replace(`#`, ``));
-        if (getPage) { getPage.innerHTML = ``; }
+    renderPage = function(page) {  // defined as "Home" or "Settings"
+        const getLayout = layout[page];
+        const getComponents = components;
+        const getContainer = document.getElementById("container");
+        if (!getLayout) {
+            return this.utils.exception(`No layout was found for the page ${page}.`, "dashboard:renderPage");
+        }
     }
+    
     
     /**
      * @production
@@ -59,10 +49,11 @@ class Dashboard {
      */
     onUpdate = function() {
         const getVersionText = document.getElementById(`sidebar-version`);
-        const getHeaderText = document.getElementById(`header-message`);
-        const getCurrentPage = this.getLandingPage();
+        const getHeaderText = document.getElementById(`message`);
+        const getCurrentPage = window.location.hash;
+        console.log(getCurrentPage)
         if (getVersionText) { getVersionText.innerText = this?.storage?.version ?? `AtmosphericX {Invalid Version}`; }
         if (getHeaderText) { if (this?.storage?.announcement) { getHeaderText.innerText = this?.storage?.announcement; } else { getHeaderText.innerText = `There are currently no announcements.`; } }
-        if (!this.static_pages.includes(getCurrentPage)) { this.loadPage(getCurrentPage); }
+        this.renderPage(getCurrentPage);
     }
 }
