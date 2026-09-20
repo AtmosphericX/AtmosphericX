1c27ad0573c142840191023473a516935dd5cbf8
k3yomi <54733885+K3YOMI@users.noreply.github.com>
2026-09-20 14:46:34 -0500

    b8.1.0 (Preconf)

--- build-tools/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ../project
-echo "Removing node_modules and package-lock.json to ensure a clean installation..."
-rm -rf node_modules package-lock.json
-npm install . --no-save
-echo "AtmosphericX dependencies installed successfully. You can now run the project using 'start.sh' under build-tools."
-read -p "Press [Enter] key to exit..."
